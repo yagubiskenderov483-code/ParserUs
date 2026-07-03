e4641bfe6902ed4fdbca51e247d15e83b7545c15
yagubiskenderov483-code <yagubiskenderov483@gmail.com>
2026-07-04 02:56:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 API_ID = 28687552
 API_HASH = "1abf9a58d0c22f62437bec89bd6b27a3"
 BOT_TOKEN = "8911943896:AAFo0BSI6FMEFJUxt-UmWtA1105sMfMWOW8"
-ADMIN_ID = 90283607
+ADMIN_ID = 90283607, 8726084830
 # ========================
 
 logging.basicConfig(level=logging.INFO)
